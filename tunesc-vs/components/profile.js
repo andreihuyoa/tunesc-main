2dfe0002068aec6a771a923964429d08f78cd62e
Ahzih <kahanapasi@gmail.com>
2023-12-13 13:04:21 +0800

    no test yet

--- tunesc-vs/components/profile.js
+++ tunesc-vs/components/profile.js
@@ -1,4 +1,5 @@
 function getUserProfile() {
+  // Log the access token for debugging
   console.log("Access Token:", window.access_token);
 
   fetch("https://api.spotify.com/v1/me", {
@@ -9,14 +10,16 @@ function getUserProfile() {
   })
     .then((response) => response.json())
     .then((data) => {
+      // Update user information
       const username = data.display_name;
       const country = data.country;
-      const email = data.email;
+      const email = data.email; // Requires user-read-email scope
       const profilePicture =
         data.images && data.images.length > 0
           ? data.images[0].url
-          : "path_to_default_image.jpg";
+          : "path_to_default_image.jpg"; // Replace with the path to your default image
 
+      // Update HTML elements
       const userNameElement = document.getElementById("user_name");
       const userCountryElement = document.getElementById("user_country");
       const userEmailElement = document.getElementById("user_email");
